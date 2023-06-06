77979282fade16cd5ac6c8355cbcb75d8dc246b1
po-miyasaka <miyasaka_kazutoshi@yahoo.co.jp>
2023-06-06 16:31:53 +1000

    test test

--- Tests/RouletteFeatureTests/RouletteFeatureTests.swift
+++ Tests/RouletteFeatureTests/RouletteFeatureTests.swift
@@ -1,11 +1,14 @@
 import XCTest
-@testable import RouletteAnalysis
+import ComposableArchitecture
+@testable import RouletteFeature
 
 final class RouletteAnalysisTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RouletteAnalysis().text, "Hello, World!")
+        let store: StoreOf<Roulette> = Store(initialState: .init(), reducer: Roulette())
+        let viewStore = ViewStore(store)
+        XCTAssertEqual(viewStore.state.settings.rule, .theStar)
     }
 }
